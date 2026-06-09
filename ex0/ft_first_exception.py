c0ca9c0112aaa99f7b66941c55b3159bbccd6605
Luis Palomino Morón <lpalominom4c@gmail.com>
2026-06-09 18:18:40 +0200

    First exercise

---
+++ ex0/ft_first_exception.py
@@ -0,0 +1,27 @@
+def input_temperature(temp_str: str) -> int:
+    return (int(temp_str))
+
+
+def test_temperature() -> None:
+    print("Input data is '25'")
+    try:
+        num1 = input_temperature('25')
+        print(f"Temperature is now {num1}°C")
+    except ValueError as e:
+        print("Caught input_temperature error:", e)
+    print("\nInput data is 'abc'")
+    try:
+        num1 = input_temperature('abc')
+        print(f"Temperature is now {num1}°C")
+    except ValueError as e:
+        print("Caught input_temperature error:", e)
+
+
+def main() -> None:
+    print("=== Garden Temperature ===\n")
+    test_temperature()
+    print("\nAll tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+    main()
